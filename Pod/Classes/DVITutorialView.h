186f4d0c6a64c5b3797ae4ea79c348c59fda5ce1
Bart van Kuik <bart@vankuik.nl>
2014-10-30 14:57:25 +0100

    Added appledoc

--- Pod/Classes/DVITutorialView.h
+++ Pod/Classes/DVITutorialView.h
@@ -1,14 +1,33 @@
 #import <UIKit/UIKit.h>
 
 
-
+/**
+ * See the README.md file for a general description.
+ */
 @interface DVITutorialView : UIView
 
+/**
+ * Set a bunch of NSStrings (or NSLocalizedStrings) with an explanation per view.
+ */
 @property (strong, nonatomic) NSArray *tutorialStrings;
-@property (strong, nonatomic)  NSArray *tutorialViews;
-@property (weak, nonatomic) UIViewController *coveredViewController;
 
+/**
+ * Set an array of views that will be exposed. If you pass a newly alloc/inited UIView (which has an empty/zero frame),
+ * then nothing will be exposed. This is useful for a welcome/exit tutorial page.
+ */
+@property (strong, nonatomic)  NSArray *tutorialViews;
+
+/**
+ * After instantiation, call this method to add the tutorial view to your viewcontroller's main view and add
+ * the constraints to make the tutorial view cover the complete frame of its superview. Alternatively, don't use this
+ * method and add a view via interface builder and set the appropriate constraints.
+ * @param view pass self.view from your viewcontroller
+ */
 - (void)addToView:(UIView*)view;
+
+/**
+ * After setting the tutorialStrings and tutorialViews properties, call this method.
+ */
 - (void)start;
 
 @end
